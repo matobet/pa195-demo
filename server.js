4a450997d33d73a945bf1c805fc8c7c9d7478e6b
Martin Betak <matobet@gmail.com>
2016-04-24 02:46:51 +0200

    Add Node.js backend
    * Store poll results in redis ordered map
    * Simplify UI

---
+++ server.js
@@ -0,0 +1,59 @@
+'use strict'
+
+var express = require('express')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+var Redis = require('ioredis')
+var redis = new Redis()
+var pubsub = new Redis()
+
+var path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+
+const options = [
+  'Transactions in Depth',
+  'Worker Queues',
+  'RPC over Reds (depends on "Queues")',
+  'Lua Stored Procedures',
+  'Distributed Locking (depends on "Lua Scripting")'
+]
+
+const initial = options.reduce((list, option) => list.concat([0, option]), [])
+redis.zadd('votes', 'nx', initial)
+
+app.get('/options', function (req, res) {
+  res.json(options)
+})
+
+app.get('/votes', function (req, res) {
+  redis.zrevrangebyscore('votes', '+inf', 0, 'withscores', (_err, votes) => {
+    let result = {}
+    for (let i = 0; i < votes.length; i += 2) {
+      result[votes[i]] = Number(votes[i + 1])
+    }
+    res.json(result)
+  })
+})
+
+pubsub.subscribe('vote')
+
+pubsub.on('message', (channel, message) => {
+  console.log('< Broadcasting vote for ' + message)
+  io.emit('vote', message)
+})
+
+io.on('connection', (socket) => {
+  const address = socket.request.connection.remoteAddress
+  console.log('> Received ws connection ' + socket.id + ' from ' + address)
+  socket.on('vote', (vote) => {
+    console.log('> Received vote for ' + vote + ' from ' + address)
+    redis.zincrby('votes', 1, vote)
+    redis.publish('vote', vote)
+  })
+})
+
+http.listen(3000, () => {
+  console.log('Listening on Port 3000')
+})
